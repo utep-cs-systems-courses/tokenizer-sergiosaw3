26835a7b6d952ae442ede2f27a8c3e1041839c75
sergiosaw3 <myfavred@gmail.com>
2021-10-02 13:20:57 -0600

    changed tokenizer.h

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,28 +1,73 @@
 #ifndef _TOKENIZER_
 #define _TOKENIZER_
-
+#include <stdlib.h>
 
 /* Return true (non-zero) if c is a whitespace characer
    (' ', '\t', or '\n').
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c)
+{
+  if(c==' ' || c=='\t' ||c=='\n')
+    {
+      return 1;
+  }
+  return 0;
+}
+
 
 /* Return true (non-zero) if c is a non-whitespace
    character (not space, tab, or newline).
    Zero terminators are not printable (therefore false) */
-int non_space_char(char c);
+int non_space_char(char c)
+{
+  if(c==' '|| c=='\t'|| c=='\n')
+    {
+      return 0;
+    }
+  return 1;
+}
 
 /* Returns a pointer to the first character of the next
    space-separated word in zero-terminated str.  Return a zero pointer if
    str does not contain any words.
    str is assumed to be pointing to a space character */
-char *word_start(char *str);
+char *word_start(char *str)
+{
+  int i;
+  char *t=(char *)malloc(sizeof(str));
+  t=str;
+  for(i=0;i<sizeof(t);i++)
+    {
+      printf("for loop works %p okay\n",t);
+      printf("inside: %c\n",t);
+      t++;
+      //printf("for loop\n");
+      ///if(non_space_char(str[i])==1)
+      //if(none_space_char(t)==1)
+      ///{
+	  // printf("word start found");
+	  ///printf("we got here\n");
+	  ///return str[i];
+	  //return t;
+      ///}
+    }
+  //free t;
+ }
 
 /* Returns a pointer terminator to the first space character or string-terminator
    following str in a zero terminated string.
    str is assumed to be pointing to a non-space character*/
-char *word_end(char *str);
-
+ char *word_end(char *str);/*
+{
+  for(i=0;i<*str;i++)
+    {
+      if(space_char(*str[i])==1)
+	{
+	  return *str[i];
+	}
+    }
+}
+/*
 /* Counts the number of space seperated words in the string argument. */
 int count_words(char *str);
 
